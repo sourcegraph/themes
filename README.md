24ac4001de950fa1cecdda1ec591b5fb371d1fb7
Quinn Slack <qslack@qslack.com>
2017-06-03 05:03:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sourcegraph color themes
+
+(Work in progress, not yet released.) This is the default color theme on [Sourcegraph.com](https://sourcegraph.com), packaged as a theme for the [Visual Studio Code](https://code.visualstudio.com) editor.
+
+[GitHub repository](https://github.com/sourcegraph/themes)
+
+## Screenshot
+
+![screenshot](https://cloud.githubusercontent.com/assets/1976/26753377/b8c24554-4819-11e7-9fae-299e4d183820.png)
---
+++ dev.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# adapted from https://github.com/dracula/visual-studio-code/blob/master/bootstrap.sh
+
+repo_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+
+attach() {
+    sourcegraph_theme_path="$( find ~/.vscode/extensions -maxdepth 1 -type d -name 'vscode-sourcegraph-theme*' )"
+    if [[ "$sourcegraph_theme_path" ]]; then
+        sourcegraph_theme_dir="$( basename "$sourcegraph_theme_path" )"
+        mkdir -p ~/.vscode/extensions/disabled
+        mv "$sourcegraph_theme_path" ~/.vscode/extensions/disabled/"$sourcegraph_theme_dir"
+    fi
+    ln -s "$repo_dir" ~/.vscode/extensions/vscode-sourcegraph-theme
+}
+
+eject() {
+    rm -f ~/.vscode/extensions/vscode-sourcegraph-theme
+    if [ -d ~/.vscode/extensions/disabled ]; then
+        disabled_path="$( find ~/.vscode/extensions/disabled -maxdepth 1 -type d -name 'vscode-sourcegraph-theme*' )"
+        sourcegraph_theme_dir="$( basename "$disabled_path" )"
+        mv "$disabled_path" ~/.vscode/extensions/"$sourcegraph_theme_dir"
+        rm -r ~/.vscode/extensions/disabled
+    fi
+}
+
+case "$1" in
+    attach)
+        attach
+        ;;
+    eject)
+        eject
+        ;;
+esac
